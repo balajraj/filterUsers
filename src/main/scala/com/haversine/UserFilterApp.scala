b349e33264508f4c2f3b24eae17a42a80e418dfb
balajraj <brajagop@gmail.com>
2018-06-20 16:27:32 +0200

    proper error message for invalid input

--- src/main/scala/com/haversine/UserFilterApp.scala
+++ src/main/scala/com/haversine/UserFilterApp.scala
@@ -4,29 +4,36 @@ package com.haversine
 import com.haversine.inputparse.ParseInput
 import com.haversine.output.{CloserUsers, ProcessOutput}
 import com.haversine.validation.LatLngValidation
+import org.slf4j.LoggerFactory
 
 import scala.io.Source
 
 object UserFilterApp {
 
-
+  val logger = LoggerFactory.getLogger("UserFilterApp")
   def main(args: Array[String]): Unit = {
 
-    if (args.length < 4) {
-      println("Please pass in radius,lat,lng,inputfile before continuing ")
-      System.exit(0)
-    }
-    val radius: Int = args(0).toInt
-    val lat: Double = args(1).toDouble
-    val lng: Double = args(2).toDouble
-    val util = new CloserUsers(lat, lng, radius)
-    val source = Source.fromFile(args(3))
-    val input = ParseInput.processInput(source)
-    val validated = LatLngValidation.validateLatLng(input)
+    try{
+      if (args.length < 4) {
+        logger.error("Please pass in radius,lat,lng,inputfile before continuing ")
+        System.exit(0)
+      }
+      val radius: Int = args(0).toInt
+      val lat: Double = args(1).toDouble
+      val lng: Double = args(2).toDouble
+      val util = new CloserUsers(lat, lng, radius)
+      val source = Source.fromFile(args(3))
+      val input = ParseInput.processInput(source)
+      val validated = LatLngValidation.validateLatLng(input)
 
-    val nearBy = util.filterUserOnDist(validated);
-    val sorted = ProcessOutput.sortListByUserId(nearBy)
-    ProcessOutput.getUserIdAndName(sorted).foreach(x => println(x._1 + " " + x._2))
+      val nearBy = util.filterUserOnDist(validated);
+      val sorted = ProcessOutput.sortListByUserId(nearBy)
+      ProcessOutput.getUserIdAndName(sorted).foreach(x => println(x._1 + " " + x._2))
+
+    }
+    catch {
+      case ex :Exception => logger.error("Invalid input passed");
+    }
 
   }
 
